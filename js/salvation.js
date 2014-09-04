8b6e0b3277805800b6cb259004be429189ebecad
Shiju Kodo <43iscoding@gmail.com>
2014-09-04 21:55:39 +0300

    planet selection

--- js/salvation.js
+++ js/salvation.js
@@ -21,6 +21,7 @@ GameState.prototype.create = function() {
     //init planets
     planetPool = initPlanetPool();
     planets = game.add.group();
+    selected = null;
     createPlanet(50, 50);
     createPlanet(200, 200);
 };
@@ -42,16 +43,45 @@ function createPlanet(x, y) {
     planet.animations.add('idle', [info.sprite], 10, true);
     planet.animations.play('idle');
     planet.inputEnabled = true;
-    planet.events.onInputDown.add(selectPlanet);
+    planet.events.onInputDown.add(onPlanetClick);
+    planet.events.onInputOver.add(onPlanetHover);
+    planet.events.onInputOut.add(onPlanetOut);
+    var overlay = game.add.sprite(x, y, 'overlays');
+    overlay.animations.add('dying', [0,1,2,3], 10, false);
+    overlay.animations.add('hover', [4], 10, true);
+    overlay.animations.add('selected', [5], 10, true);
+    overlay.animations.add('idle', [6], 10, true);
+    overlay.animations.play('idle');
+    planet.overlay = overlay;
     return planet;
 }
 
-function selectPlanet(planet, pointer) {
-    console.log(planet.name);
+var selected = null;
+
+function onPlanetClick(planet, pointer) {
+    planets.forEach(function(planet) {
+        planet.overlay.animations.play('idle');
+    }, this);
+    planet.overlay.animations.play('selected');
+    selected = planet;
+}
+
+function onPlanetHover(planet, pointer) {
+    if (planet == selected) return;
+    planet.overlay.animations.play('hover');
+}
+
+function onPlanetOut(planet, pointer) {
+    if (planet == selected) return;
+    planet.overlay.animations.play('idle');
 }
 
 GameState.prototype.update = function() {
 
+};
+
+GameState.prototype.render = function() {
+    game.debug.text('Selected: ' + (selected != null ? selected.name : 'None'), 500, 50);
 };
 
 var game = new Phaser.Game(1136, 640, Phaser.AUTO, 'game');
