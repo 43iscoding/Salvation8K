aa8eb81a2921c9264790bb59575df872aee973c1
Shiju Kodo <43iscoding@gmail.com>
2014-09-06 01:45:44 +0300

    Overlay fix, portal rotation, planet names from JSON

--- js/salvation.js
+++ js/salvation.js
@@ -4,6 +4,7 @@ window.HEIGHT = 640;
 var GameState = function(game) {};
 
 GameState.prototype.preload = function() {
+    game.load.text('planetNames', 'res/planets.json');
     game.load.image('mainmenu','res/mainmenu.png');
     game.load.image('mainmenu','res/mainmenuDemo.png');
     game.load.image('noise.png', 'res/noise.png');
@@ -14,6 +15,7 @@ GameState.prototype.preload = function() {
 };
 
 var planets;
+var portal;
 
 var planetPool;
 
@@ -29,6 +31,7 @@ GameState.prototype.create = function() {
     selected = null;
     createPlanet(50, 50);
     createPlanet(200, 200);
+    portal = createPortal(400, 100);
 };
 
 function initBackground() {
@@ -57,10 +60,10 @@ function getRandomStarColor() {
 }
 
 function initPlanetPool() {
-    var names = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'];
+    var names = JSON.parse(game.cache.getText('planetNames'))['planets'];
     names = Phaser.Utils.shuffle(names);
     var pool = [];
-    for (var i = 0; i < PLANET_TYPES; i++) {
+    for (var i = 0; i < names.length && i < PLANET_TYPES; i++) {
         pool.push({name : names.pop(), sprite : i});
     }
     return Phaser.Utils.shuffle(pool);
@@ -76,46 +79,69 @@ function createPlanet(x, y) {
     planet.events.onInputDown.add(onPlanetClick);
     planet.events.onInputOver.add(onPlanetHover);
     planet.events.onInputOut.add(onPlanetOut);
+    planet.anchor.setTo(0.5);
     var overlay = game.add.sprite(x, y, 'overlays');
-    overlay.animations.add('dying', [0,1,2,3], 10, false);
     overlay.animations.add('hover', [4], 10, true);
     overlay.animations.add('selected', [5], 10, true);
-    overlay.animations.add('idle', [6], 10, true);
-    overlay.animations.play('idle');
+    overlay.visible = false;
+    overlay.anchor.setTo(0.5);
     planet.overlay = overlay;
     return planet;
 }
 
+function createPortal(x, y) {
+    var portal = planets.create(x, y, 'planets');
+    portal.name = 'Wormhole';
+    portal.animations.add('idle', [16], 10, true);
+    portal.animations.play('idle');
+    portal.inputEnabled = true;
+    portal.events.onInputDown.add(onPlanetClick);
+    portal.events.onInputOver.add(onPlanetHover);
+    portal.events.onInputOut.add(onPlanetOut);
+    portal.anchor.setTo(0.5);
+    var overlay = game.add.sprite(x, y, 'overlays');
+    overlay.animations.add('dying', [0,1,2,3], 10, false);
+    overlay.animations.add('hover', [6], 10, true);
+    overlay.animations.add('selected', [7], 10, true);
+    overlay.visible = false;
+    overlay.anchor.setTo(0.5);
+    portal.overlay = overlay;
+    return portal;
+}
+
 var selected = null;
 
-function onSpaceClick(bg, pointer) {
+function onSpaceClick(g, pointer) {
     deselectAll();
     selected = null;
 }
 function onPlanetClick(planet, pointer) {
     deselectAll();
+    planet.overlay.visible = true;
     planet.overlay.animations.play('selected');
     selected = planet;
 }
 
 function deselectAll() {
     planets.forEach(function(planet) {
-        planet.overlay.animations.play('idle');
+        planet.overlay.visible = false;
     }, this);
 }
 
 function onPlanetHover(planet, pointer) {
     if (planet == selected) return;
+    planet.overlay.visible = true;
     planet.overlay.animations.play('hover');
 }
 
 function onPlanetOut(planet, pointer) {
     if (planet == selected) return;
-    planet.overlay.animations.play('idle');
+    planet.overlay.visible = false;
 }
 
 GameState.prototype.update = function() {
-
+    portal.rotation += 0.01;
+    portal.overlay.rotation += 0.01;
 };
 
 GameState.prototype.render = function() {
